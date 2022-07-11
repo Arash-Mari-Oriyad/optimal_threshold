82217069dfaea80c571a0f9e7623a56dcd589f61
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2022-07-11 20:27:53 +0430

    update check results

--- experiments/check_results.py
+++ experiments/check_results.py
@@ -1,9 +1,9 @@
-report_1 = open('cpp/exhaustive_search_1/results/1000_4/prediction_report.txt', 'r')
+report_1 = open('cpp/exhaustive_search_1/results/1000_1/threshold_accuracy_report.txt', 'r')
 report_1_lines = report_1.readlines()
 report_1_lines = [line.strip().split(' ') for line in report_1_lines]
 report_1_lines = [[float(line[0]), float(line[1])] for line in report_1_lines]
 
-report_2 = open('cpp/ilp/results/1000_4/prediction_report.txt', 'r')
+report_2 = open('cpp/ilp/results/1000_1/threshold_accuracy_report.txt', 'r')
 report_2_lines = report_2.readlines()
 report_2_lines = [line.strip().split(' ') for line in report_2_lines]
 report_2_lines = [[float(line[0]), float(line[1])] for line in report_2_lines]
